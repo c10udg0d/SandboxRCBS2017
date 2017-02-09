4c3be4b3c6b8bc3729824ed157a3994660381111
José Carlos Paiva Rodrigues <jose.rodrigues@linkconsulting.com>
2017-02-09 19:07:35 +0000

    Oauth

--- twitter-notes/index.js
+++ twitter-notes/index.js
@@ -1,4 +1,4 @@
-console.log ("Hello World!!");
+/*console.log ("Hello World!!");
 var express = require('express');
 var app = express();
 
@@ -8,4 +8,34 @@ app.get("/",function(req,res){
 
 app.listen(8080, function() {
     console.log("Express Server a correr no porto 8080");
+});*/
+
+var url = require('url');
+var express = require('express');
+var authenticator = require('./authenticator');
+var config = require('./config');
+var app = express();
+
+// Add cookie parsing functionality to our Express app
+app.use(require('cookie-parser')());
+
+// Take user to Twitter's login page
+app.get('/auth/twitter', authenticator.redirectToTwitterLoginPage);
+
+// This is the callback url that the user is redirected to after signing in
+app.get(url.parse(config.oauth_callback).path, function(req, res) {
+	authenticator.authenticate(req, res, function(err) {
+		if (err) {
+			console.log(err);
+            res.send("You Failed");
+			res.sendStatus(401);
+		} else {
+			res.send("Authentication Successful");
+		}
+	});
+});
+
+// Start listening for requests
+app.listen(config.port, function() {
+	console.log("Listening on port " + config.port);
 });
